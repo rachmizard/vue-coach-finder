818ea9b3a614862f4aae811b9b98ea2dfcd47ac6
Rachmizard <rachmizard11072000@gmail.com>
2020-10-25 16:53:55 +0700

    add filters

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,61 @@
+<template>
+    <base-card title="Filter">
+        <div class="checkbox">
+            <label>
+            <input id="frontend" type="checkbox" checked @change="setFilter"> Frontend
+            </label>
+        </div>
+        <div class="checkbox">
+            <label>
+            <input id="backend" type="checkbox" checked @change="setFilter"> Backend
+            </label>
+        </div>
+        <div class="checkbox">
+            <label>
+            <input id="uiux" type="checkbox" checked @change="setFilter"> UI/UX
+            </label>
+        </div>
+        <div class="checkbox">
+            <label>
+            <input id="mobile" type="checkbox" checked @change="setFilter"> Mobile
+            </label>
+        </div>
+        <div class="checkbox">
+            <label>
+            <input id="sa" type="checkbox" checked @change="setFilter"> System Analyst
+            </label>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search..">
+        </div>
+    </base-card>
+</template>
+<script>
+export default {
+    emits: ['change-filter'],
+    data() {
+        return {
+            filters: {
+                frontend: true,
+                backend: true,
+                uiux: true,
+                sa: true,
+                mobile: true,
+            }
+        }
+    },
+    methods: {
+        setFilter(event) {
+            const inputId = event.target.id
+            const isActive = event.target.checked;
+            const updatedFilters = {
+                ...this.filters,
+                [inputId]: isActive
+            }  
+
+            this.filters = updatedFilters;
+            this.$emit('change-filter', updatedFilters);
+        }
+    }
+}
+</script>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="row">
     <div class="col-md-2">
-      <base-card title="Filter">
-        <base-button mode="btn btn-sm btn-info">Refresh</base-button>
-      </base-card>
+      <coach-filter @change-filter="setFilters"></coach-filter>
     </div>
     <div class="col-md-10">
-      <div class="row">
+      <div class="row" v-if="coaches.length > 0">
         <coach-item
           v-for="(coach, index) in coaches"
           :key="index"
           :coach="coach"
         ></coach-item>
+      </div>
+      <div class="row" v-else>
+        <p class="text-muted text-center">Data is not found.</p>
       </div>
     </div>
   </div>
@@ -19,15 +20,51 @@
 
 <script>
 import CoachItem from "./../../components/coaches/CoachItem.vue";
+import CoachFilter from "./../../components/coaches/CoachFilter.vue";
 
 export default {
   name: "CoachesList",
+  data() {
+    return {
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        uiux: true,
+        sa: true,
+        mobile: true
+      },
+    };
+  },
   components: {
     coachItem: CoachItem,
+    coachFilter: CoachFilter,
   },
   computed: {
     coaches() {
-      return this.$store.getters.getCoaches;
+      const data = this.$store.getters.getCoaches;
+      return data.filter(coach => {
+        if(this.activeFilters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if(this.activeFilters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if(this.activeFilters.mobile && coach.areas.includes('mobile')) {
+          return true;
+        }
+        if(this.activeFilters.uiux && coach.areas.includes('ui/ux')) {
+          return true;
+        }
+        if(this.activeFilters.sa && coach.areas.includes('system analyst')) {
+          return true;
+        }
+        return false;
+      })
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.activeFilters = updatedFilters
     },
   },
 };
